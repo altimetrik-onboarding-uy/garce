2e72a8b47c4ff4505a0ef9b0e6131ee8d33341a9
ALTIMETRIK\garce <garce@altimetrik.com>
2018-07-04 18:30:28 -0300

    Part 3 of Compensations with fixes to bugs from Part2.

--- src/aura/CompensationsTab/CompensationsTabController.js
+++ src/aura/CompensationsTab/CompensationsTabController.js
@@ -2,18 +2,61 @@
     doInit: function (component, event, helper) {
         // Create the action
 
-        helper.loadTable(component, 'All');
+        helper.loadTable(component, 'All', 'All');
         helper.loadfilter(component);
 
     },
     filterTable: function (component, event, helper) {
 
-        helper.loadTable(component, component.get('v.selectedValue'));
+        helper.loadTable(component, component.get('v.selectedValue'), component.get('v.submittedValue'));
     },
 
-    handleSaveSuccess: function (component, event, helper) {
+    handleSelectCompensation: function (component, event, helper) {
+        console.log('Estoy escuchando este nuevo');
+        var add = event.getParam("add");
+        var compensation = event.getParam("compensation");
+        var selectedComps = component.get("v.selectedCompensations");
 
-        console.log('el evento');
+        if (add) {
+            selectedComps.push(compensation);
 
+        }
+        else {
+            helper.removeComp(component, compensation);
+
+        }
+    },
+
+    sendtoSubmitt: function (component, event, helper) {
+        var list = component.get("v.selectedCompensations");
+        if (list.length > 0) {
+            helper.sendSubmit(component, list);
+            helper.loadTable(component, 'All', 'All');
+        } else {
+            alert('No compensation(s) to submit');
+        }
+
+    },
+
+    downloadCsv: function (component, event, helper) {
+
+
+        var comps = component.get("v.compensations");
+
+        // call the helper function which "return" the CSV data as a String   
+        var csv = helper.convertArrayOfObjectsToCSV(component, comps);
+        if (csv == null) { return; }
+
+        // ####--code for create a temp. <a> html tag [link tag] for download the CSV file--####     
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_self'; // 
+        hiddenElement.download = 'Compensations.csv';  // CSV file Name* you can change it.[only name not .csv] 
+        document.body.appendChild(hiddenElement); // Required for FireFox browser
+        hiddenElement.click(); // using click() js function to download csv file
     }
+
+
+
+
 })
--- src/aura/compensationEdit/compensationEditController.js
+++ src/aura/compensationEdit/compensationEditController.js
@@ -8,9 +8,24 @@
 			editRecordEvent.fire();
 			
 			
-		}
+		},
 
 	
-		
+		save : function(component, event, helper) {
+
+			console.log('estoy ecuchando');
+		},
+
+		selectCompensation: function(component, event, helper) {
+			
+			var compensation = component.get("v.compensation");
+			var updateEvent = component.getEvent("selectcompensation");
+			var add= component.find("selectionCheck").get("v.value");
+			
+			updateEvent.setParams({ "compensation": compensation, "add":add });
+			updateEvent.fire();
+		}
+
+
 	
 })
